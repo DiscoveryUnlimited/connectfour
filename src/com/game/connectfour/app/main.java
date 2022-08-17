6787c5e6daf0ab1769d96ded5ee037edabcae426
Weijun124 <98248068+Weijun124@users.noreply.github.com>
2022-08-17 09:33:26 -0700

    Update main.java

--- src/com/game/connectfour/app/main.java
+++ src/com/game/connectfour/app/main.java
@@ -2,7 +2,7 @@ package com.game.connectfour.app;
 
 import com.game.connectfour.ConnectFourApp;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         ConnectFourApp app = new ConnectFourApp();
         app.execute();
